5aa1ff04487a88acc312b5029a47c39c90d31137
Tatsiana Kobets <tatsiana.kobets@yandex.ru>
2023-10-24 08:46:31 +0300

    Initial commit

---
+++ src/main/java/com/example/resttemplatespring/RestTemplateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.resttemplatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resttemplatespring/RestTemplateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resttemplatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
